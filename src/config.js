9f2f9ab2d76c96663caad7c9deb69aeb7b47dba9
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-02-29 20:19:44 +0000

    add ignores and edit config.js

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ module.exports = {
 
     // ssl object is either null or { key: fs.readFileSync('path/to/key'), cert: fs.readFileSync('path/to/cert') }
     // for more info, see https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
-    ssl: null,
+    ssl: { key: fs.readFileSync('path/to/key'), cert: fs.readFileSync('path/to/cert') },
 
     // this function's return object will determine how the client url rewriting will work.
     // set them differently from bindingAddress and port if rammerhead is being served
