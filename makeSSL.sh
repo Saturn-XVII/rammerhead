7ca964ae0cfabd7041070dc60ac318c8c09ffb1c
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-04-17 15:21:31 +0000

    mommmmm

--- makeSSL.sh
+++ makeSSL.sh
@@ -1,2 +1,2 @@
-# download openSSL before hand
+# download openSSL beforehand
 mkdir SSL && cd SSL && openssl genrsa -out key.pem && openssl req -new -key key.pem -out csr.pem && openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem
